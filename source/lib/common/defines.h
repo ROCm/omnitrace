53ea2252777720a50fb77957a75272e0a9ffdc13
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2022-06-22 14:14:10 -0500

    libomnitrace uses common headers
    - JOIN(...) uses ::omnitrace::common::join instead of ::tim::mpl::apply<>::join

--- source/lib/common/defines.h
+++ source/lib/common/defines.h
@@ -22,12 +22,38 @@
 
 #pragma once
 
-#define OMNITRACE_ATTRIBUTE(...)   __attribute__((__VA_ARGS__))
-#define OMNITRACE_VISIBILITY(MODE) OMNITRACE_ATTRIBUTE(visibility(MODE))
-#define OMNITRACE_PUBLIC_API       OMNITRACE_VISIBILITY("default")
-#define OMNITRACE_HIDDEN_API       OMNITRACE_VISIBILITY("hidden")
-#define OMNITRACE_WEAK_API         OMNITRACE_ATTRIBUTE(weak)
-#define OMNITRACE_INLINE           OMNITRACE_ATTRIBUTE(__always_inline__)
+#define OMNITRACE_ATTRIBUTE(...)    __attribute__((__VA_ARGS__))
+#define OMNITRACE_VISIBILITY(MODE)  OMNITRACE_ATTRIBUTE(visibility(MODE))
+#define OMNITRACE_PUBLIC_API        OMNITRACE_VISIBILITY("default")
+#define OMNITRACE_HIDDEN_API        OMNITRACE_VISIBILITY("hidden")
+#define OMNITRACE_INLINE            OMNITRACE_ATTRIBUTE(always_inline) inline
+#define OMNITRACE_NOINLINE          OMNITRACE_ATTRIBUTE(noinline)
+#define OMNITRACE_HOT               OMNITRACE_ATTRIBUTE(hot)
+#define OMNITRACE_CONST             OMNITRACE_ATTRIBUTE(const)
+#define OMNITRACE_PURE              OMNITRACE_ATTRIBUTE(pure)
+#define OMNITRACE_PACKED            OMNITRACE_ATTRIBUTE(__packed__)
+#define OMNITRACE_PACKED_ALIGN(VAL) OMNITRACE_PACKED OMNITRACE_ATTRIBUTE(__aligned__(VAL))
+
+#if defined(OMNITRACE_CI) && OMNITRACE_CI > 0
+#    if defined(NDEBUG)
+#        undef NDEBUG
+#    endif
+#    if !defined(DEBUG)
+#        define DEBUG 1
+#    endif
+#    if defined(__cplusplus)
+#        include <cassert>
+#    else
+#        include <assert.h>
+#    endif
+#endif
+
+#define OMNITRACE_STRINGIZE(X)           OMNITRACE_STRINGIZE2(X)
+#define OMNITRACE_STRINGIZE2(X)          #X
+#define OMNITRACE_VAR_NAME_COMBINE(X, Y) X##Y
+#define OMNITRACE_VARIABLE(Y)            OMNITRACE_VAR_NAME_COMBINE(_omni_var_, Y)
+#define OMNITRACE_LINESTR                OMNITRACE_STRINGIZE(__LINE__)
+#define OMNITRACE_ESC(...)               __VA_ARGS__
 
 #if defined(__cplusplus)
 #    if !defined(OMNITRACE_FOLD_EXPRESSION)
