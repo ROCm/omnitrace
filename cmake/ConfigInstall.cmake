95913c713588d3ed4e4eb2d80bda32048280b13e
Jonathan R. Madsen <jrmadsen@users.noreply.github.com>
2022-08-08 08:38:38 -0500

    Update python install + build-tree setup (#128)
    - When one python version is used, install to proper lib/pythonX.Y/site-packages
    - config files, etc. in build tree resemble the install-tree

--- cmake/ConfigInstall.cmake
+++ cmake/ConfigInstall.cmake
@@ -21,19 +21,20 @@ set(PROJECT_BUILD_TARGETS user dl)
 
 configure_package_config_file(
     ${PROJECT_SOURCE_DIR}/cmake/Templates/${PROJECT_NAME}-config.cmake.in
-    ${PROJECT_BINARY_DIR}/install-tree/${PROJECT_NAME}-config.cmake
+    ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_DATAROOTDIR}/cmake/${PROJECT_NAME}/${PROJECT_NAME}-config.cmake
     INSTALL_DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/cmake/omnitrace
     INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}
     PATH_VARS PROJECT_INSTALL_DIR INCLUDE_INSTALL_DIR LIB_INSTALL_DIR)
 
 write_basic_package_version_file(
-    ${PROJECT_BINARY_DIR}/install-tree/${PROJECT_NAME}-version.cmake
+    ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_DATAROOTDIR}/cmake/${PROJECT_NAME}/${PROJECT_NAME}-version.cmake
     VERSION ${PROJECT_VERSION}
-    COMPATIBILITY SameMajorVersion)
+    COMPATIBILITY SameMinorVersion)
 
 install(
-    FILES ${PROJECT_BINARY_DIR}/install-tree/${PROJECT_NAME}-config.cmake
-          ${PROJECT_BINARY_DIR}/install-tree/${PROJECT_NAME}-version.cmake
+    FILES
+        ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_DATAROOTDIR}/cmake/${PROJECT_NAME}/${PROJECT_NAME}-config.cmake
+        ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_DATAROOTDIR}/cmake/${PROJECT_NAME}/${PROJECT_NAME}-version.cmake
     DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/cmake/${PROJECT_NAME}
     OPTIONAL)
 
