fd8eccf8a5646f4053925242ef54352495025a30
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2022-09-28 15:53:45 -0500

    Fix OMPT initialization

--- source/lib/omnitrace/library/ompt.cpp
+++ source/lib/omnitrace/library/ompt.cpp
@@ -20,6 +20,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include "api.hpp"
+#include "library/common.hpp"
+#include "library/config.hpp"
+#include "library/debug.hpp"
 #include "library/defines.hpp"
 
 #include <timemory/defines.h>
@@ -96,6 +100,16 @@ ompt_start_tool(unsigned int omp_version, const char* runtime_version)
 
     OMNITRACE_METADATA("OMP_VERSION", omp_version);
     OMNITRACE_METADATA("OMP_RUNTIME_VERSION", runtime_version);
+
+    if(!omnitrace::settings_are_configured())
+    {
+        OMNITRACE_BASIC_WARNING(
+            0,
+            "[%s] invoked before omnitrace was initialized. In instrumentation mode, "
+            "settings exported to the environment have not been propagated yet...\n",
+            __FUNCTION__);
+        omnitrace::configure_settings();
+    }
 
     static bool _use_ompt       = omnitrace::config::get_use_ompt();
     static auto ompt_initialize = [](ompt_function_lookup_t lookup,
