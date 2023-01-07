8bce1c1a8513bea75f73d418942bed3bcb157599
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2023-01-06 19:45:37 -0600

    process-causal-json.py script
    - will be used later for testing

---
+++ tests/process-causal-json.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+import argparse
+from collections import OrderedDict
+
+
+class experiment_data(object):
+    def __init__(self, _speedup):
+        self.speedup = _speedup
+        self.duration = []
+
+    def __iadd__(self, _val):
+        self.duration += [float(_val)]
+
+    def __len__(self):
+        return len(self.duration)
+
+    def __eq__(self, rhs):
+        return self.speedup == rhs.speedup
+
+    def __neq__(self, rhs):
+        return not self == rhs
+
+    def __lt__(self, rhs):
+        return self.speedup < rhs.speedup
+
+    def mean(self):
+        return sum(self.duration) / float(len(self.duration))
+
+
+class line_speedup(object):
+    def __init__(self, _name="", _exp_data=None, _exp_base=None):
+        self.name = _name
+        self.data = _exp_data
+        self.base = _exp_base
+
+    def get(self):
+        if self.data is None or self.base is None:
+            return 0.0
+        return ((self.base.mean() - self.data.mean()) / self.base.mean()) * 100
+
+    def __str__(self):
+        if self.data is None or self.base is None:
+            return f"{self.name}"
+        _line_speedup = self.get()
+        return f"[{self.name}][{self.data.speedup:3}] speedup: {_line_speedup:6.1f} %"
+
+    def __eq__(self, rhs):
+        return self.name == rhs.name and self.data == rhs.data and self.base == rhs.base
+
+    def __neq__(self, rhs):
+        return not self == rhs
+
+    def __lt__(self, rhs):
+        if self.name != rhs.name:
+            return self.name < rhs.name
+        elif self.data != rhs.data:
+            return self.data < rhs.data
+        elif self.base != rhs.base:
+            return self.base < rhs.base
+        return False
+
+
+def find_or_insert(_data, _value):
+    if _value not in _data:
+        _data[_value] = experiment_data(_value)
+    return _data[_value]
+
+
+def longest_common_prefix(a):
+    # if size is 0, return empty string
+    # if size is 1, return first string
+    if len(a) < 2:
+        return "" if len(a) == 0 else a[0]
+
+    # sort the array of strings
+    a.sort()
+
+    # find the minimum length from first and last string
+    end = min(len(a[0]), len(a[-1]))
+
+    # find the common prefix between the first and last string
+    i = 0
+    while i < end and a[0][i] == a[-1][i]:
+        i += 1
+
+    pre = a[0][0:i]
+    return pre
+
+
+def process_data(data, _data):
+    if not _data:
+        return data
+
+    for record in _data["omnitrace"]["causal"]["records"]:
+        for exp in record["experiments"]:
+            _speedup = exp["virtual_speedup"]
+            _duration = exp["duration"]
+            _file = exp["selection"]["info"]["file"]
+            _line = exp["selection"]["info"]["line"]
+            _func = exp["selection"]["info"]["dfunc"]
+            _sym_addr = exp["selection"]["symbol_address"]
+            _selected = ":".join([_file, f"{_line}"]) if _sym_addr == 0 else _func
+            if _selected not in data:
+                data[_selected] = {}
+            _weight = 1
+            for pts in exp["progress_points"]:
+                _name = pts["name"]
+                if "delta" in pts:
+                    _delt = pts["delta"]
+                    _arrive = pts["arrival"]
+                    _depart = pts["departure"]
+                    if _delt > 0:
+                        _weight = _delt if _delt > _weight else _weight
+                    _delt = _arrive - _depart
+                    if _delt > 0:
+                        _weight = _delt if _delt > _weight else _weight
+                else:
+                    _delt = pts["laps"]
+                    _weight = _delt if _delt > _weight else _weight
+            itr = find_or_insert(data[_selected], _speedup)
+            itr += float(_duration) / float(_weight)
+
+    return data
+
+
+def compute_speedups(_data):
+    data = {}
+    _names = []
+    for selected, ditr in _data.items():
+        data[selected] = OrderedDict(sorted(ditr.items()))
+        if ":" in selected:
+            _name = os.path.dirname(selected.split(":")[0])
+            if _name not in _names:
+                _names.append(_name)
+
+    lcp = longest_common_prefix(_names)
+
+    ret = []
+    for selected, ditr in data.items():
+        if 0 not in ditr.keys():
+            print(f"missing baseline data for {selected}...")
+            continue
+        selected = selected.replace(lcp, "", 1) if len(lcp) > 0 else selected
+        _baseline = ditr[0].mean()
+        for speedup, itr in ditr.items():
+            if speedup != itr.speedup:
+                raise ValueError(f"in {selected}: {speedup} != {itr.speedup}")
+            _val = line_speedup(selected, itr, ditr[0])
+            ret.append(_val)
+
+    _last_name = None
+    for itr in ret:
+        if itr.name != _last_name:
+            print("")
+        print(f"{itr}")
+        _last_name = itr.name
+
+    return ret
+
+
+if __name__ == "__main__":
+    data = {}
+    for inp in sys.argv[1:]:
+        with open(inp, "r") as f:
+            inp_data = json.load(f)
+        data = process_data(data, inp_data)
+
+    compute_speedups(data)
