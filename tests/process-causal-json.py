550eba360b953886aaec873c7e539602186350d4
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2023-01-09 01:27:09 -0600

    process-causal-json script

--- tests/process-causal-json.py
+++ tests/process-causal-json.py
@@ -32,8 +32,9 @@ class experiment_data(object):
 
 
 class line_speedup(object):
-    def __init__(self, _name="", _exp_data=None, _exp_base=None):
+    def __init__(self, _name="", _prog="", _exp_data=None, _exp_base=None):
         self.name = _name
+        self.prog = _prog
         self.data = _exp_data
         self.base = _exp_base
 
@@ -46,10 +47,15 @@ class line_speedup(object):
         if self.data is None or self.base is None:
             return f"{self.name}"
         _line_speedup = self.get()
-        return f"[{self.name}][{self.data.speedup:3}] speedup: {_line_speedup:6.1f} %"
+        return f"[{self.name}][{self.prog}][{self.data.speedup:3}] speedup: {_line_speedup:6.1f} %"
 
     def __eq__(self, rhs):
-        return self.name == rhs.name and self.data == rhs.data and self.base == rhs.base
+        return (
+            self.name == rhs.name
+            and self.prog == rhs.prog
+            and self.data == rhs.data
+            and self.base == rhs.base
+        )
 
     def __neq__(self, rhs):
         return not self == rhs
@@ -57,6 +63,8 @@ class line_speedup(object):
     def __lt__(self, rhs):
         if self.name != rhs.name:
             return self.name < rhs.name
+        elif self.prog != rhs.prog:
+            return self.prog < rhs.prog
         elif self.data != rhs.data:
             return self.data < rhs.data
         elif self.base != rhs.base:
@@ -70,14 +78,20 @@ def find_or_insert(_data, _value):
     return _data[_value]
 
 
-def longest_common_prefix(a):
+def longest_common_prefix(_inp):
+    a = list(set(_inp))
+
     # if size is 0, return empty string
     # if size is 1, return first string
     if len(a) < 2:
-        return "" if len(a) == 0 else a[0]
+        return ""
 
     # sort the array of strings
     a.sort()
+
+    # if all the same, return empty string
+    if a[0] == a[-1]:
+        return ""
 
     # find the minimum length from first and last string
     end = min(len(a[0]), len(a[-1]))
@@ -106,58 +120,70 @@ def process_data(data, _data):
             _selected = ":".join([_file, f"{_line}"]) if _sym_addr == 0 else _func
             if _selected not in data:
                 data[_selected] = {}
-            _weight = 1
             for pts in exp["progress_points"]:
                 _name = pts["name"]
+                if _name not in data[_selected]:
+                    data[_selected][_name] = {}
+                itr = find_or_insert(data[_selected][_name], _speedup)
                 if "delta" in pts:
                     _delt = pts["delta"]
-                    _arrive = pts["arrival"]
-                    _depart = pts["departure"]
                     if _delt > 0:
-                        _weight = _delt if _delt > _weight else _weight
-                    _delt = _arrive - _depart
-                    if _delt > 0:
-                        _weight = _delt if _delt > _weight else _weight
+                        itr += float(_duration) / float(_delt)
+                    else:
+                        _diff = pts["arrival"] - pts["departure"] + 1
+                        _rate = pts["arrival"] / float(_duration)
+                        itr += float(_diff) / float(_rate)
                 else:
                     _delt = pts["laps"]
-                    _weight = _delt if _delt > _weight else _weight
-            itr = find_or_insert(data[_selected], _speedup)
-            itr += float(_duration) / float(_weight)
 
     return data
 
 
 def compute_speedups(_data):
     data = {}
-    _names = []
-    for selected, ditr in _data.items():
-        data[selected] = OrderedDict(sorted(ditr.items()))
-        if ":" in selected:
-            _name = os.path.dirname(selected.split(":")[0])
-            if _name not in _names:
-                _names.append(_name)
+    for selected, pitr in _data.items():
+        if selected not in data:
+            data[selected] = {}
 
-    lcp = longest_common_prefix(_names)
+        for progpt, ditr in pitr.items():
+            data[selected][progpt] = OrderedDict(sorted(ditr.items()))
+
+    from os.path import dirname
 
     ret = []
-    for selected, ditr in data.items():
-        if 0 not in ditr.keys():
-            print(f"missing baseline data for {selected}...")
-            continue
-        selected = selected.replace(lcp, "", 1) if len(lcp) > 0 else selected
-        _baseline = ditr[0].mean()
-        for speedup, itr in ditr.items():
-            if speedup != itr.speedup:
-                raise ValueError(f"in {selected}: {speedup} != {itr.speedup}")
-            _val = line_speedup(selected, itr, ditr[0])
-            ret.append(_val)
+    _slcp = longest_common_prefix(
+        [dirname(x.split(":")[0]) if ":" in x else x for x in _data.keys()]
+    )
+    for selected, pitr in _data.items():
+        selected = selected.replace(_slcp, "", 1) if len(_slcp) > 0 else selected
+        if len(selected) > 0 and selected[0] == "/":
+            selected = selected[1:]
+        _plcp = longest_common_prefix(
+            [dirname(x.split(":")[0]) if ":" in x else x for x in pitr.keys()]
+        )
+        for progpt, ditr in pitr.items():
+            progpt = progpt.replace(_plcp, "", 1) if len(_plcp) > 0 else progpt
+            if len(progpt) > 0 and progpt[0] == "/":
+                progpt = progpt[1:]
+            if 0 not in ditr.keys():
+                print(f"missing baseline data for {progpt} in {selected}...")
+                continue
+            _baseline = ditr[0].mean()
+            for speedup, itr in ditr.items():
+                if speedup != itr.speedup:
+                    raise ValueError(f"in {selected}: {speedup} != {itr.speedup}")
+                _val = line_speedup(selected, progpt, itr, ditr[0])
+                ret.append(_val)
 
+    ret.sort()
     _last_name = None
+    _last_prog = None
     for itr in ret:
-        if itr.name != _last_name:
+        if itr.name != _last_name or itr.prog != _last_prog:
             print("")
         print(f"{itr}")
         _last_name = itr.name
+        _last_prog = itr.prog
 
     return ret
 
