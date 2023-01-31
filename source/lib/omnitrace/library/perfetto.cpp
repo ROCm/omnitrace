222e5a9b0f85bca6b38e2ff2bee62736debb75ca
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2023-01-31 17:56:01 -0600

    perfetto updates
    - Remove OMNITRACE_CUSTOM_DATA_SOURCE CMake option
    - move tracing::get_perfetto_config and tracing::get_perfetto_session to perfetto.cpp

--- source/lib/omnitrace/library/perfetto.cpp
+++ source/lib/omnitrace/library/perfetto.cpp
@@ -28,37 +28,40 @@ namespace omnitrace
 {
 namespace perfetto
 {
+auto&
+get_config()
+{
+    static auto _v = ::perfetto::TraceConfig{};
+    return _v;
+}
+
+auto&
+get_session()
+{
+    static auto _v = std::unique_ptr<::perfetto::TracingSession>{};
+    return _v;
+}
+
 void
 setup()
 {
     auto  args            = ::perfetto::TracingInitArgs{};
     auto  track_event_cfg = ::perfetto::protos::gen::TrackEventConfig{};
-    auto& cfg             = tracing::get_perfetto_config();
+    auto& cfg             = get_config();
 
     // environment settings
-    auto shmem_size_hint = get_perfetto_shmem_size_hint();
-    auto buffer_size     = get_perfetto_buffer_size();
+    auto shmem_size_hint = config::get_perfetto_shmem_size_hint();
+    auto buffer_size     = config::get_perfetto_buffer_size();
 
     auto _policy =
-        get_perfetto_fill_policy() == "discard"
+        config::get_perfetto_fill_policy() == "discard"
             ? ::perfetto::protos::gen::TraceConfig_BufferConfig_FillPolicy_DISCARD
             : ::perfetto::protos::gen::TraceConfig_BufferConfig_FillPolicy_RING_BUFFER;
     auto* buffer_config = cfg.add_buffers();
     buffer_config->set_size_kb(buffer_size);
     buffer_config->set_fill_policy(_policy);
 
-    std::set<std::string> _available_categories = {};
-    std::set<std::string> _disabled_categories  = {};
-    for(auto itr : { OMNITRACE_PERFETTO_CATEGORIES })
-        _available_categories.emplace(itr.name);
-    auto _enabled_categories = config::get_perfetto_categories();
-    for(const auto& itr : _available_categories)
-    {
-        if(!_enabled_categories.empty() && _enabled_categories.count(itr) == 0)
-            _disabled_categories.emplace(itr);
-    }
-
-    for(const auto& itr : _disabled_categories)
+    for(const auto& itr : config::get_disabled_categories())
     {
         OMNITRACE_VERBOSE_F(1, "Disabling perfetto track event category: %s\n",
                             itr.c_str());
@@ -81,31 +84,19 @@ setup()
 void
 start()
 {
-#if defined(CUSTOM_DATA_SOURCE)
-    // Add the following:
-    ::perfetto::DataSourceDescriptor dsd{};
-    dsd.set_name("com.example.custom_data_source");
-    CustomDataSource::Register(dsd);
-    auto* ds_cfg = cfg.add_data_sources()->mutable_config();
-    ds_cfg->set_name("com.example.custom_data_source");
-    CustomDataSource::Trace([](CustomDataSource::TraceContext ctx) {
-        auto packet = ctx.NewTracePacket();
-        packet->set_timestamp(::perfetto::TrackEvent::GetTraceTimeNs());
-        packet->set_for_testing()->set_str("Hello world!");
-        PRINT_HERE("%s", "Trace");
-    });
-#endif
-    auto& cfg             = tracing::get_perfetto_config();
-    auto& tracing_session = tracing::get_perfetto_session();
+    auto& cfg             = get_config();
+    auto& tracing_session = get_session();
     tracing_session       = ::perfetto::Tracing::NewTrace();
     tracing_session->Setup(cfg);
     tracing_session->StartBlocking();
 }
 }  // namespace perfetto
+
+std::unique_ptr<::perfetto::TracingSession>&
+get_perfetto_session()
+{
+    return ::omnitrace::perfetto::get_session();
+}
 }  // namespace omnitrace
 
 PERFETTO_TRACK_EVENT_STATIC_STORAGE();
-
-#if defined(CUSTOM_DATA_SOURCE)
-PERFETTO_DEFINE_DATA_SOURCE_STATIC_MEMBERS(CustomDataSource);
-#endif
