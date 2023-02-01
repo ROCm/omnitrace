8828efb657f40b26b01bf3d5c61d72b4c914ece5
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2023-01-31 20:39:50 -0600

    sampling + backtrace_metrics
    - rework backtrace_metrics to support categories

--- source/lib/omnitrace/library/components/backtrace_metrics.hpp
+++ source/lib/omnitrace/library/components/backtrace_metrics.hpp
@@ -34,6 +34,7 @@
 #include <timemory/components/papi/types.hpp>
 #include <timemory/macros/language.hpp>
 #include <timemory/mpl/concepts.hpp>
+#include <timemory/utility/type_list.hpp>
 #include <timemory/variadic/types.hpp>
 
 #include <array>
@@ -45,11 +46,14 @@
 
 namespace omnitrace
 {
+template <typename... Tp>
+using type_list = ::tim::type_list<Tp...>;
+
 namespace component
 {
 struct backtrace_metrics
 : tim::component::empty_base
-, tim::concepts::component
+, concepts::component
 {
     static constexpr size_t num_hw_counters = TIMEMORY_PAPI_ARRAY_SIZE;
 
@@ -59,6 +63,13 @@ struct backtrace_metrics
     using hw_counter_data_t = typename hw_counters::value_type;
     using system_clock      = std::chrono::system_clock;
     using system_time_point = typename system_clock::time_point;
+
+    using categories_t =
+        type_list<category::thread_cpu_time, category::thread_peak_memory,
+                  category::thread_context_switch, category::thread_page_fault,
+                  category::thread_hardware_counter, hw_counters>;
+    static constexpr size_t num_categories = std::tuple_size<categories_t>::value;
+    using valid_array_t                    = std::bitset<num_categories>;
 
     static std::string label();
     static std::string description();
@@ -72,9 +83,15 @@ struct backtrace_metrics
     backtrace_metrics& operator=(backtrace_metrics&&) noexcept = default;
 
     static void                     configure(bool, int64_t _tid = threading::get_id());
-    static void                     init_perfetto(int64_t _tid);
-    static void                     fini_perfetto(int64_t _tid);
+    static void                     init_perfetto(int64_t _tid, valid_array_t);
+    static void                     fini_perfetto(int64_t _tid, valid_array_t);
     static std::vector<std::string> get_hw_counter_labels(int64_t);
+
+    template <typename Tp>
+    static bool get_valid(Tp, valid_array_t);
+
+    template <typename Tp>
+    static bool get_valid(type_list<Tp>, valid_array_t);
 
     static void start();
     static void stop();
@@ -82,6 +99,15 @@ struct backtrace_metrics
     void        post_process(int64_t _tid, const backtrace* _bt,
                              const backtrace_metrics* _last) const;
 
+    explicit operator bool() const { return m_valid.any(); }
+
+    template <typename Tp>
+    bool operator()(Tp) const;
+
+    template <typename Tp>
+    bool operator()(type_list<Tp>) const;
+
+    auto        get_valid() const { return m_valid; }
     auto        get_cpu_timestamp() const { return m_cpu; }
     auto        get_peak_memory() const { return m_mem_peak; }
     auto        get_context_switches() const { return m_ctx_swch; }
@@ -91,12 +117,44 @@ struct backtrace_metrics
     void post_process_perfetto(int64_t _tid, uint64_t _ts) const;
 
 private:
+    valid_array_t     m_valid      = {};
     int64_t           m_cpu        = 0;
     int64_t           m_mem_peak   = 0;
     int64_t           m_ctx_swch   = 0;
     int64_t           m_page_flt   = 0;
     hw_counter_data_t m_hw_counter = {};
 };
+
+template <typename Tp>
+bool
+backtrace_metrics::get_valid(type_list<Tp>, valid_array_t _valid)
+{
+    constexpr auto idx = tim::index_of<Tp, categories_t>::value;
+    return _valid.test(idx);
+}
+
+template <typename Tp>
+bool backtrace_metrics::operator()(type_list<Tp>) const
+{
+    static_assert(!concepts::is_type_listing<Tp>::value,
+                  "Error! invalid call with tuple");
+
+    constexpr auto idx = tim::index_of<Tp, categories_t>::value;
+    return m_valid.test(idx);
+}
+
+template <typename Tp>
+bool
+backtrace_metrics::get_valid(Tp, valid_array_t _valid)
+{
+    return get_valid(type_list<Tp>{}, _valid);
+}
+
+template <typename Tp>
+bool backtrace_metrics::operator()(Tp) const
+{
+    return (*this)(type_list<Tp>{});
+}
 }  // namespace component
 }  // namespace omnitrace
 
