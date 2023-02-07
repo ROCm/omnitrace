d274649146011e360d19dfc0c6c6d6d4d4ac8997
Jonathan R. Madsen <jrmadsen@users.noreply.github.com>
2023-02-07 07:57:24 -0600

    Disable storing env in sampling::shutdown

--- source/lib/omnitrace/library/sampling.cpp
+++ source/lib/omnitrace/library/sampling.cpp
@@ -675,8 +675,8 @@ std::set<int>
 shutdown()
 {
     bool _is_child =
-        get_env("OMNITRACE_CHILD_PROCESS", false) ||
-        (get_env("OMNITRACE_ROOT_PROCESS", process::get_id()) != process::get_id());
+        get_env("OMNITRACE_CHILD_PROCESS", false, false) ||
+        (get_env("OMNITRACE_ROOT_PROCESS", process::get_id(), false) != process::get_id());
 
     if(_is_child)
     {
