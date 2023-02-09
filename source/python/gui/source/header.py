c14b555b9f7c1015ec040fda3cd2ba7008909830
Jonathan R. Madsen <jrmadsen@users.noreply.github.com>
2023-02-09 13:20:47 -0600

    Omnitrace causal viewer initial implementation (#242)
    * initial gui commit with workloads folder
    
    * Insert Workload directory, and Drag/drop works, options/values update correctly
    
    * min points is now a slider and updates on each workload. still doesnt filter
    
    * min points filter, now filters
    
    * points filter works with point selection
    
    * globs workloads in current dir and one level down
    
    * regex filters
    
    * regex filtering works
    
    * changed index tuples in dataframes
    
    * added correct indexing to data
    
    * Remove pycache files
    
    * ignore .pyc files
    
    * format python files
    
    * deleted unnecessary workload files
    
    * remove unused python scripts
    
    * add requirements.txt
    
    * remove unused imports + unnecessary code
    
    * Relocated source files
    
    * support pip installation
    
    * changed layout to handle more data, updated to new json format
    
    * return of smooth lines
    
    * scrollable histogram
    
    * fixed for throughput
    
    * formatting
    
    * miscellaneous fixes
    
    * precalc sorting values for less work in callback
    
    - added compute_sorts to precalculate dataframe sort columns
    -renamed columns in compute_speedups to same format as output
    -removed sorting algorithms from callback
    -fixed func_regex and exp_regex to use func_list and exp_list
    
    * Min/Max sort works
    
    -min/max sort fixed,
    -For some reason dataframe is calculated twice...
    
    * Merge with new compute scripts
    
    -create settings if nonexistent
    -write current path, and clil flag to settings after argparse
    -add new flags from validation script
    -use impact avg from validation scripts
    -use new classes for calculating throughput/latency/impact
    -uses regex from cli for progress points/experiments
    
    * Remove outdated code
    
    -removed unused/commented code
    -fixed import errors
    
    * reformatting
    
    * Pasting path in gui fix
    
    -fixed parser default values
    -pasting path to workload folder in box fixed to new format
    
    * drag and drop file fix
    
    * Updated python headers + v0.1.0 + fixed --version
    
    - set version to 0.1.0
    - moved source/python/gui/VERSION to source/python/gui/source/VERSION
    - populated __init__.py with __version__
    - populated all python files with author, copyright, license, maintainer, and status
    - fixed usage from installation
    
    * formatting
    
    ---------
    
    Co-authored-by: JoseSantosAMD <Josantos@amd.com>

---
+++ source/python/gui/source/header.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python3
+# MIT License
+#
+# Copyright (c) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import sys
+import dash_daq as daq
+import dash_bootstrap_components as dbc
+
+from dash import html, dash_table, dcc
+from matplotlib.style import available
+
+
+# List all the unique column values for desired column in df, 'target_col'
+def list_unique(orig_list, is_numeric):
+    list_set = set(orig_list)
+    unique_list = list(list_set)
+    if is_numeric:
+        unique_list.sort()
+    return unique_list
+
+
+def filePath():
+    return html.Div(
+        className="filter",
+        children=[
+            html.Li(
+                dcc.Input(
+                    id="file-path",
+                    placeholder="Insert Workload directory",
+                    type="text",
+                    debounce=True,
+                    style={
+                        "width": "100%",
+                        "height": "40%",
+                        "lineHeight": "60px",
+                        "borderWidth": "1px",
+                        #'borderStyle': 'dashed',
+                        "borderRadius": "5px",
+                        "textAlign": "center",
+                    },
+                )
+            )
+        ],
+    )
+
+
+def function_filter(_id, _placeholder):
+    return html.Li(
+        className="filter",
+        children=[
+            html.Li(
+                dcc.Input(
+                    id=_id,
+                    placeholder=_placeholder,
+                    type="text",
+                    debounce=True,
+                    style={
+                        "width": "100%",
+                        "height": "40%",
+                        "lineHeight": "60px",
+                        "borderWidth": "1px",
+                        #'borderStyle': 'dashed',
+                        "borderRadius": "5px",
+                        "textAlign": "center",
+                    },
+                )
+            )
+        ],
+    )
+
+
+def uploadFile():
+    return html.Div(
+        className="filter",
+        children=[
+            html.Li(
+                children=[
+                    # drag and drop
+                    dcc.Upload(
+                        id="upload-drag",
+                        children=["Drag and Drop or ", html.A("Select a File")],
+                        style={
+                            "width": "100%",
+                            "height": "40%",
+                            "lineHeight": "40%",
+                            "borderWidth": "1px",
+                            "borderStyle": "dashed",
+                            "borderRadius": "5px",
+                            "textAlign": "center",
+                        },
+                    )
+                ]
+            )
+        ],
+    )
+
+
+def minPoints(name, values):
+    return html.Li(
+        className="filter",
+        # style={#'width': '100%',
+        #'height': '50px'},
+        children=[
+            html.Div(
+                style={
+                    "width": "200px",
+                    "position": "relative",
+                    "display": "inline-block",
+                    "list-style": "none",
+                },
+                children=[
+                    html.A(
+                        className="smoothscroll",
+                        children=["Min Points:"],
+                    ),
+                ],
+            ),
+            html.Div(
+                style={
+                    "width": "200px",
+                    "padding-top": "10px",
+                    "vertical-align": "middle",
+                    "position": "relative",
+                    "display": "inline-block",
+                    "list-style": "none",
+                },
+                children=[
+                    daq.Slider(
+                        min=0,
+                        max=values,
+                        step=1,
+                        value=1,
+                        id="points-filt",
+                        handleLabel={"showCurrentValue": True, "label": "VALUE"},
+                        size=200,
+                    )
+                ],
+            ),
+        ],
+    )
+
+
+def sortBy(name, values, filter, style_):
+    return html.Li(
+        className="filter",
+        children=[
+            html.Div(
+                children=[
+                    html.A(
+                        className="smoothscroll",
+                        children=[name + ":"],
+                    ),
+                    dcc.Dropdown(
+                        list_unique(
+                            values,
+                            True,
+                        ),
+                        id=name + "-filt",
+                        multi=True,
+                        value=filter,
+                        placeholder="ALL",
+                        clearable=False,
+                        style=style_,
+                    ),
+                ]
+            )
+        ],
+    )
+
+
+def reportBug():
+    return html.Div(
+        className="nav-right",
+        children=[
+            html.Li(
+                children=[
+                    # Report bug button
+                    html.A(
+                        href="",
+                        children=[
+                            html.Button(
+                                className="report",
+                                children=["Report Bug"],
+                            )
+                        ],
+                    )
+                ]
+            )
+        ],
+    )
+
+
+def get_header(raw_pmc, dropDownMenuItems, input_filters, kernel_names):
+    children_ = [
+        html.Nav(
+            id="nav-wrap",
+            children=[
+                html.Ul(
+                    id="nav",
+                    children=[
+                        html.Div(
+                            className="nav-left",
+                            children=[
+                                dbc.DropdownMenu(
+                                    dropDownMenuItems,
+                                    label="Menu",
+                                    menu_variant="dark",
+                                ),
+                            ],
+                        ),
+                    ],
+                )
+            ],
+        ),
+    ]
+
+    for filter in input_filters:
+        header_nav = children_[0].children[0].children
+        if filter["type"] == "int":
+            header_nav.append(minPoints(filter["Name"], filter["values"]))
+        elif filter["type"] == "Name":
+            header_nav.append(
+                sortBy(
+                    filter["Name"],
+                    filter["values"],
+                    filter["filter"],
+                    {
+                        "width": "200px",  # TODO: Change these widths to % rather than fixed value
+                        "height": "34px",
+                    },
+                )
+            )
+        # elif filter["type"] == "Function Name":
+        #    header_nav.append(
+        #        function_filter(
+        #            filter["Name"],
+        #            filter["values"],
+        #            filter["filter"],
+        #            {
+        #                "width": "200px",  # TODO: Change these widths to % rather than fixed value
+        #                "height": "34px",
+        #            },
+        #        )
+        # id    )
+        else:
+            print("type not supported")
+            # sys.exit(1)
+    header_nav = children_[0].children[0].children
+    header_nav.append(function_filter("function_regex", "Funtion/line regex"))
+    header_nav.append(function_filter("exp_regex", "Experiment regex"))
+    header_nav.append(reportBug())
+    # header_nav.append(minPoints())
+
+    header_nav.append(filePath())
+    header_nav.append(uploadFile())
+
+    return html.Header(id="home", children=children_)
