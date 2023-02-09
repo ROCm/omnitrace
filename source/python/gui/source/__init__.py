95335355a7bb3ef478d7dc4a2f23a3290f2f9580
Jonathan R. Madsen <jrmadsen@users.noreply.github.com>
2023-02-09 13:20:47 -0600

    Omnitrace causal viewer initial implementation (#242)
    * initial gui commit with workloads folder
    
    * Insert Workload directory, and Drag/drop works, options/values update correctly
    
    * min points is now a slider and updates on each workload. still doesnt filter
    
    * min points filter, now filters
    
    * points filter works with point selection
    
    * globs workloads in current dir and one level down
    
    * regex filters
    
    * regex filtering works
    
    * changed index tuples in dataframes
    
    * added correct indexing to data
    
    * Remove pycache files
    
    * ignore .pyc files
    
    * format python files
    
    * deleted unnecessary workload files
    
    * remove unused python scripts
    
    * add requirements.txt
    
    * remove unused imports + unnecessary code
    
    * Relocated source files
    
    * support pip installation
    
    * changed layout to handle more data, updated to new json format
    
    * return of smooth lines
    
    * scrollable histogram
    
    * fixed for throughput
    
    * formatting
    
    * miscellaneous fixes
    
    * precalc sorting values for less work in callback
    
    - added compute_sorts to precalculate dataframe sort columns
    -renamed columns in compute_speedups to same format as output
    -removed sorting algorithms from callback
    -fixed func_regex and exp_regex to use func_list and exp_list
    
    * Min/Max sort works
    
    -min/max sort fixed,
    -For some reason dataframe is calculated twice...
    
    * Merge with new compute scripts
    
    -create settings if nonexistent
    -write current path, and clil flag to settings after argparse
    -add new flags from validation script
    -use impact avg from validation scripts
    -use new classes for calculating throughput/latency/impact
    -uses regex from cli for progress points/experiments
    
    * Remove outdated code
    
    -removed unused/commented code
    -fixed import errors
    
    * reformatting
    
    * Pasting path in gui fix
    
    -fixed parser default values
    -pasting path to workload folder in box fixed to new format
    
    * drag and drop file fix
    
    * Updated python headers + v0.1.0 + fixed --version
    
    - set version to 0.1.0
    - moved source/python/gui/VERSION to source/python/gui/source/VERSION
    - populated __init__.py with __version__
    - populated all python files with author, copyright, license, maintainer, and status
    - fixed usage from installation
    
    * formatting
    
    ---------
    
    Co-authored-by: JoseSantosAMD <Josantos@amd.com>

---
+++ source/python/gui/source/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# MIT License
+#
+# Copyright (c) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import absolute_import
+
+__author__ = "AMD Research"
+__copyright__ = "Copyright 2023, Advanced Micro Devices, Inc."
+__license__ = "MIT"
+__maintainer__ = "AMD Research"
+__status__ = "Development"
+
+
+def _get_version():
+    import os
+    from pathlib import Path
+
+    this_dir = Path(__file__).resolve().parent
+    ver_path = os.path.join(f"{this_dir}", "VERSION")
+    if os.path.exists(ver_path):
+        with open(ver_path, "r") as f:
+            return f.read().strip("\n")
+    return "???"
+
+
+__version__ = _get_version()
