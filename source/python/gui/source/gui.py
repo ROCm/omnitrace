180f006ded1b9f41d94182a8fcbc69fd2d4243b9
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2023-04-03 15:53:31 -0500

    GUI: gui.py updates
    - remove unused imports
    - fix light mode
    - sync initial y range of all causal plots
    - fix error bars for causal data
    - set x-ticks to 5
    - set y-ticks to 10
    - only display top 99% of samples
    - separate global declarations and assignments to global values
    - remove unused variable assignments
    - fix mislabeled function_regex and exp_regex
    - change if X == False to if not X

--- source/python/gui/source/gui.py
+++ source/python/gui/source/gui.py
@@ -31,24 +31,15 @@ __status__ = "Development"
 
 import glob
 import re
-import sys
-import copy
 import base64
 import os
 import pandas as pd
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
-from re import L
-from selectors import EpollSelector
-from matplotlib.axis import XAxis
-from numpy import append
-from dash.dash_table import FormatTemplate
-from dash.dash_table.Format import Format, Scheme, Symbol
-from dash import html, dash_table
+from dash import html
 from dash.dependencies import Input, Output, State
 from dash import dcc, ctx
-from os.path import exists
 
 from .header import get_header
 from .parser import parse_files
@@ -63,6 +54,7 @@ global_samples = pd.DataFrame()
 global_input_filters = None
 workload_path = ""
 verbose = 0
+text_color = "white"
 
 pd.set_option(
     "mode.chained_assignment", None
@@ -89,13 +81,13 @@ def build_line_graph():
     layout1 = html.Div(
         id="graph_all",
         className="graph",
-        children=[html.H4("All Causal Profiles", style={"color": "white"})],
+        children=[html.H4("All Causal Profiles", style={"color": text_color})],
     )
 
     layout2 = html.Div(
         id="graph_select",
         className="graph",
-        children=[html.H4("Call Stack Sample Histogram", style={"color": "white"})],
+        children=[html.H4("Call Stack Sample Histogram", style={"color": text_color})],
     )
 
     return layout1, layout2
@@ -131,7 +123,27 @@ def update_line_graph(
             [colors_df, pd.DataFrame({"progress points": [prog], "color": [color * 100]})]
         )
 
-    causalLayout = [html.H4("Selected Causal Profiles", style={"color": "white"})]
+    causalLayout = [html.H4("Selected Causal Profiles", style={"color": text_color})]
+
+    # all the workload should start off with the same Y range so make comparisons easier
+    y_ranges = [0, 0]
+    for speedup, speedup_err in zip(mask["program speedup"], mask["speedup err"]):
+        y_ranges = [
+            min([y_ranges[0], speedup - speedup_err]),
+            max([y_ranges[1], speedup + speedup_err]),
+        ]
+    # add some extra room
+    y_ranges = [
+        (round((y_ranges[0] - 5) / 10) * 10) - 1,
+        (round((y_ranges[1] + 5) / 10) * 10) + 1,
+    ]
+    # enforce an absolute lower bound and an abosulte upper bound
+    # bottom of y_range will not be greater than -1 and not less than -125
+    # top of y_range will not be less than 1 and not greater than 125
+    y_ranges = [
+        max([min([-1, y_ranges[0]]), -125]),
+        min([max([1, y_ranges[1]]), 125]),
+    ]
 
     for point in list(mask.point.unique()):
         subplots = go.Figure()
@@ -148,20 +160,29 @@ def update_line_graph(
                     x=sub_data_prog["line speedup"],
                     y=sub_data_prog["program speedup"],
                     error_y=dict(
-                        type="percent", array=sub_data_prog["impact err"].tolist()
+                        type="data", array=sub_data_prog["speedup err"].tolist()
                     ),
                     line_shape="spline",
                     name=prog,
                     mode="lines+markers",
-                )
-            ).update_layout(xaxis={"title": x_label}, yaxis={"title": "Program Speedup"})
-        causalLayout.append(html.H4(point, style={"color": "white"}))
+                ),
+            ).update_xaxes(dtick=5).update_yaxes(range=y_ranges, dtick=10).update_layout(
+                xaxis={"title": x_label},
+                yaxis={"title": "Program Speedup"},
+                height=600,
+            )
+        causalLayout.append(html.H4(point, style={"color": text_color}))
         causalLayout.append(dcc.Graph(figure=subplots))
+
+    # compute the location with the most samples
+    _max_sample = samples.groupby(["location"]).sum()["count"].max()
+    # don't display samples < 1% of the peak
+    samples = samples[samples["count"] >= 0.01 * _max_sample]
 
     fig3 = px.bar(samples, x="location", y="count", height=1200)
 
     samplesLayout = [
-        html.H4("Call Stack Sample Histogram", style={"color": "white"}),
+        html.H4("Call Stack Sample Histogram", style={"color": text_color}),
         dcc.Graph(figure=fig3),
     ]
 
@@ -202,22 +223,22 @@ def build_causal_layout(
     global verbose
     global global_data
     global global_samples
-    global_data = data
-    global_samples = samples
     global global_input_filters
-    global_input_filters = input_filters
     global workload_path
-    workload_path = path_to_dir
+    global text_color
 
+    global_input_filters = input_filters
+    global_data = data
+    text_color = "black" if light_mode else "white"
+    workload_path = path_to_dir
     verbose = verbosity
+    global_samples = samples
 
     dropDownMenuItems = [
         dbc.DropdownMenuItem("Overview", header=True),
         dbc.DropdownMenuItem("All Causal Profiles"),
         dbc.DropdownMenuItem("Selected Causal Profiles"),
     ]
-
-    inital_min_points = 3
 
     app.layout = html.Div(
         style={"backgroundColor": "rgb(255, 255, 255)"}
@@ -247,8 +268,8 @@ def build_causal_layout(
         [Input("refresh", "n_clicks")],
         [Input("Sort by-filt", "value")],
         [Input("Select Workload-filt", "value")],
-        [Input("function_regex", "value")],
-        [Input("exp_regex", "value")],
+        [Input("experiment_regex", "value")],
+        [Input("progpt_regex", "value")],
         [Input("points-filt", "value")],
         [Input("file-path", "value")],
         [Input("upload-drag", "contents")],
@@ -260,8 +281,8 @@ def build_causal_layout(
         refresh,
         sort_filter,
         workload_filter,
-        func_regex,
-        exp_regex,
+        experiment_regex,
+        progpt_regex,
         num_points,
         _workload_path,
         contentsList,
@@ -276,8 +297,8 @@ def build_causal_layout(
         # change to if debug
         if verbose >= 3:
             print("Sort by is ", sort_filter)
-            print("funcRegex is ", func_regex)
-            print("expRegex is ", exp_regex)
+            print("experiment_regex is ", experiment_regex)
+            print("progress_point_regex is ", progpt_regex)
             print("points is: ", num_points)
             print("workload_path is: ", workload_path)
             print("selected workloads are:", workload_filter)
@@ -301,7 +322,7 @@ def build_causal_layout(
                 _files = glob.glob(os.path.join(_workload_path, "*.json"))
                 # subfiles = glob.glob(os.path.join(workload_path, "*/*.coz")) +
                 subfiles = glob.glob(os.path.join(_workload_path, "*/*.json"))
-                metadata = glob.glob(os.path.join(_workload_path, "*/metadata*.json"))
+                # metadata = glob.glob(os.path.join(_workload_path, "*/metadata*.json"))
                 files = _files + subfiles
                 workload_path = files
             global_data, global_samples, global_filenames = parse_files(
@@ -359,16 +380,16 @@ def build_causal_layout(
                 header = get_header(dropDownMenuItems, global_input_filters)
 
         # runs when function or experiment regex is changed, takes numPoints into account as well
-        elif func_regex is not None or exp_regex is not None:
-            if global_data.empty == False:
-                if func_regex is not None:
-                    p = re.compile(func_regex, flags=0)
+        elif experiment_regex is not None or progpt_regex is not None:
+            if not global_data.empty:
+                if experiment_regex is not None:
+                    p = re.compile(experiment_regex, flags=0)
 
                     func_list = [
                         s for s in list(global_data["point"].unique()) if p.match(s)
                     ]
-                if exp_regex is not None:
-                    p = re.compile(exp_regex, flags=0)
+                if progpt_regex is not None:
+                    p = re.compile(progpt_regex, flags=0)
 
                     exp_list = [
                         s
@@ -398,7 +419,7 @@ def build_causal_layout(
                 files=files, verbose=verbose
             )
 
-            if global_data.empty == False:
+            if not global_data.empty:
                 max_points = global_data.point.value_counts().max().max()
 
                 # reset input_filters
@@ -424,7 +445,7 @@ def build_causal_layout(
         else:
             func_list = []
             exp_list = []
-            if global_data.empty == False:
+            if not global_data.empty:
                 if verbose == 3:
                     print(global_data)
                 func_list = sorted(list(global_data.point.unique()))
@@ -438,7 +459,7 @@ def build_causal_layout(
                     global_samples,
                     workload_filter,
                 )
-        if screen_data.empty == False:
+        if not screen_data.empty:
             if verbose == 2:
                 print_speedup_info(screen_data)
             elif verbose >= 3:
