b9f1688f79dd1226c6b3215ecc8ac015d976459e
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2023-04-12 00:31:47 -0500

    causal backtrace updates
    - reintroduce select_ival behavior

--- source/lib/omnitrace/library/causal/components/backtrace.cpp
+++ source/lib/omnitrace/library/causal/components/backtrace.cpp
@@ -146,8 +146,7 @@ backtrace::sample(int _sig)
     constexpr size_t  depth        = ::omnitrace::causal::unwind_depth;
     constexpr int64_t ignore_depth = ::omnitrace::causal::unwind_offset;
     constexpr size_t  select_init  = std::numeric_limits<size_t>::max();
-    // constexpr size_t  select_ival  = 5;  // interval at which realtime signal
-    // contributes
+    constexpr size_t  select_ival  = 5;  // interval at which realtime signal contributes
 
     // update the last sample for backtrace signal(s) even when in use
     static thread_local size_t _protect_flag = 0;
@@ -213,8 +212,8 @@ backtrace::sample(int _sig)
             // line/function... despite the line/function executing in between the
             // the cputime signals. This is rare but has been observed
             //
-            // if(_select_count == 0 && _select_zeros >= select_ival)
-            if(_select_count == 0) _set_current_selection(m_stack);
+            if(_select_count == 0 && _select_zeros >= select_ival)
+                _set_current_selection(m_stack);
         }
     }
     else
