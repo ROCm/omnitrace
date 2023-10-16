2e581e2a10ab42596890649d665ae16d5566c0bb
Jonathan R. Madsen <jrmadsen@users.noreply.github.com>
2023-10-16 10:39:18 -0500

    Installers for ROCm 5.7, Python 3.12 + Remove DEB and RPM installers (#313)
    * Dockerfile update
    
    - Python 3.12 support
    
    * Bump version to 1.10.4
    
    * Update docker scripts
    
    - Support Python 3.12
    - Set RETRY to 1 if less than 1
    - Support ROCm 5.7
    
    * Update scripts/build-release.sh
    
    - Default to python 3.6-3.12 (i.e. add Python 3.12)
    
    * Update cpack workflow
    
    - Packaging for ROCm 5.7
      - Ubuntu 20.04
      - Ubuntu 22.04
      - OpenSUSE 15.4
      - RHEL 8.7
      - RHEL 9.1
    - Packaging for older ROCms (by request)
      - RHEL 8.7 + ROCm 5.3
      - OpenSUSE 15.3 + ROCm 5.2
      - OpenSUSE 15.4 + ROCm 5.2
      - OpenSUSE 15.4 + ROCm 5.3
    - Remove DEB and RPM installers
      - Only generate STGZ installers
    
    * Update cpack workflow
    
    - disable uploading DEB and RPM artifacts

--- docker/build-docker-ci.sh
+++ docker/build-docker-ci.sh
@@ -8,7 +8,7 @@ set -e
 : ${NJOBS=$(nproc)}
 : ${ELFUTILS_VERSION:=0.186}
 : ${BOOST_VERSION:=1.79.0}
-: ${PYTHON_VERSIONS:="6 7 8 9 10 11"}
+: ${PYTHON_VERSIONS:="6 7 8 9 10 11 12"}
 : ${PUSH:=0}
 : ${PULL:=--pull}
 
