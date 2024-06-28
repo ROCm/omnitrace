fc44f2c0b4ce25ea0dedf0681f1a3693f958cad3
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-06-28 14:38:29 -0600

    Run doxysphinx in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,8 @@ external_toc_path = "./sphinx/_toc.yml"
 
 docs_core = ROCmDocs(html_title)
 docs_core.setup()
+docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
+docs_core.enable_api_reference()
 
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
