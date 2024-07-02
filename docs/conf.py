19a905391498bfa420aed0123cbbb10324b89e16
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-07-02 10:41:17 -0600

    Update Doxyfile
    Generate from source .in

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,5 @@ docs_core.setup()
 docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
 docs_core.enable_api_reference()
 
-
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
