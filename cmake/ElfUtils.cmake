b0b1dca21ea524839c7cc2e109c7ab399f812fa5
David Galiffi <David.Galiffi@amd.com>
2024-07-10 10:34:18 -0400

    Add ElfUtils and BinUtils Download URL Overrides (#358)
    * Add CMake CACHE Variable ElfUtils_DOWNLOAD_URL
    
    Used to override the default URL to download ElfUtils from.
    Useful for internal builds
    
    Also, include a mirror to fallback to if the override URL fails.
    
    * Update timemory submodule
    
    Updating to include the BINUTIL_DOWNLOAD_URL override cmake
    variable.
    
    ---------
    
    Signed-off-by: David Galiffi <David.Galiffi@amd.com>

--- cmake/ElfUtils.cmake
+++ cmake/ElfUtils.cmake
@@ -102,7 +102,8 @@ set(_eu_build_byproducts "${_eu_root}/lib/libdw${CMAKE_STATIC_LIBRARY_SUFFIX}"
 externalproject_add(
     omnitrace-elfutils-build
     PREFIX ${PROJECT_BINARY_DIR}/external/elfutils
-    URL https://sourceware.org/elfutils/ftp/${ElfUtils_DOWNLOAD_VERSION}/elfutils-${ElfUtils_DOWNLOAD_VERSION}.tar.bz2
+    URL ${ElfUtils_DOWNLOAD_URL}
+        "https://sourceware.org/elfutils/ftp/${ElfUtils_DOWNLOAD_VERSION}/elfutils-${ElfUtils_DOWNLOAD_VERSION}.tar.bz2"
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND
         ${CMAKE_COMMAND} -E env CC=${ElfUtils_C_COMPILER}
