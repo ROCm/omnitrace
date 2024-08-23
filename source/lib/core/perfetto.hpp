f3cdbfaf54682309e2942f3174e321c24c261d4a
Aleksandar Janicijevic <Aleksandar.Janicijevic@amd.com>
2024-08-23 15:45:34 -0400

    Fixed compile with perfetto v46.0

--- source/lib/core/perfetto.hpp
+++ source/lib/core/perfetto.hpp
@@ -109,7 +109,7 @@ perfetto_counter_track<Tp>::emplace(size_t _idx, const std::string& _v,
     auto        _index     = _track_data.size();
     auto&       _name      = _name_data.emplace_back(std::make_unique<std::string>(_v));
     const char* _unit_name = (_units && strlen(_units) > 0) ? _units : nullptr;
-    _track_data.emplace_back(::perfetto::CounterTrack{ _name->c_str() }
+    _track_data.emplace_back(::perfetto::CounterTrack{ ::perfetto::DynamicString{_name->c_str()} }
                                  .set_unit_name(_unit_name)
                                  .set_category(_category)
                                  .set_unit_multiplier(_mult)
